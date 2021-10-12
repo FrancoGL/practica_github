eda0621d3097fc275ed03e376a1a6e94e609e715
Alan <88910353+AlanRimoli@users.noreply.github.com>
2021-10-12 20:44:54 -0300

    Practica Git
    Alan: Agrego sout al main y agrego la clase "pa-TOMAR"

--- java/src/java/Main.java
+++ java/src/java/Main.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package java;
 
-/**
- *
- * @author gonza
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Alumnos en este proyecto:" +
+                "1. Alan" +
+                "2." +
+                "3." +
+                "4." +
+                "5.");
+
     }
     
 }
---
+++ java/src/java/Patomar.java
@@ -0,0 +1,11 @@
+package java;
+
+public class Patomar {
+
+    public static void main(String[] args) {
+
+        System.out.println("CLASE CREADA POR ALAN.");
+
+    }
+
+}
